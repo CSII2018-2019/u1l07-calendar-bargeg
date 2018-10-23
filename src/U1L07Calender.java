41df82fb34d5fff1604c88711017c7d2737dd2a9
gennabarge <gennabarge@10.0.0.12>
2018-10-23 06:39:41 -0400

    FINAL Calendar assignment- fixed the # of days in the print statement

--- src/U1L07Calender.java
+++ src/U1L07Calender.java
@@ -1,3 +1,4 @@
+//Genna Barge- U1L07 Calender 
 import javax.swing.JOptionPane;
 
 public class U1L07Calender {
@@ -6,12 +7,13 @@ public class U1L07Calender {
 		
 		// TODO Auto-generated method stub
 		int i = 0;
-		String [] months = {"Jan", "Feb", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+		String [] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 		String [] daysOfWeek = {"monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"};
 		int [] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; 		
 		
 		String input = JOptionPane.showInputDialog("Enter a number day from 1-360");
 		int numberDay = Integer.parseInt(input);
+		int initialNumber = numberDay; 
 		
 		int weekDay = numberDay % 7;
 		//enter that as the index for the loop
@@ -24,7 +26,7 @@ public class U1L07Calender {
 		}
 		String finalMonth = months[i];
 		int finalDay = numberDay + 1;
-		System.out.println(numberDay + " days from January first is: " + finalMonth + " " + finalDay + " which is a " + finalWeekDay);
+		System.out.println(initialNumber + " days from January first is: " + finalMonth + " " + finalDay + " which is a " + finalWeekDay);
 				
 		
 			
